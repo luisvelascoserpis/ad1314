c992de887912717cbd959e0ba1fef3f0951291cf
luisvelascoserpis <luis.velasco.serpis@gmail.com>
2013-10-10 13:09:20 +0200

    PTreeView añadida acción Editar (se activa cuando hay alguna fila seleccionada. Por ahora sólo muestra un MessageDialog)

--- PTreeView/PTreeView/MainWindow.cs
+++ PTreeView/PTreeView/MainWindow.cs
@@ -38,16 +38,34 @@ public partial class MainWindow: Gtk.Window
 		
 		treeView.Model = listStore;
 		
-		treeView.Selection.Changed += delegate {
-			TreeIter treeIter;
-			Console.WriteLine ("============");
-			if (treeView.Selection.GetSelected (out treeIter)) {
-				Console.WriteLine ("listStore.GetPath(treeIter)=" + listStore.GetPath(treeIter) );
-				Console.WriteLine ("listStore.GetValue(treeIter, 0)=" + listStore.GetValue(treeIter, 0));
-				Console.WriteLine ("listStore.GetValue(treeIter, 1)=" + listStore.GetValue(treeIter, 1));
-			} else
-				Console.WriteLine ("Ninguno seleccionado");
+		editAction.Sensitive = false;
+		
+		editAction.Activated += delegate {
+			MessageDialog messageDialog = new MessageDialog(this,
+                DialogFlags.DestroyWithParent,
+                MessageType.Info,
+                ButtonsType.Ok,
+                "Este es el mensaje informativo de las 13:00.\n La clase se ha acabado.");
+			messageDialog.Title = "Este es el título del mensaje";
+			messageDialog.Run ();
+			messageDialog.Destroy ();
 		};
+		
+		treeView.Selection.Changed += delegate {
+			editAction.Sensitive = treeView.Selection.CountSelectedRows() > 0;
+		};
+		
+		//treeView.Selection.CountSelectedRows()
+//		treeView.Selection.Changed += delegate {
+//			TreeIter treeIter;
+//			Console.WriteLine ("============");
+//			if (treeView.Selection.GetSelected (out treeIter)) {
+//				Console.WriteLine ("listStore.GetPath(treeIter)=" + listStore.GetPath(treeIter) );
+//				Console.WriteLine ("listStore.GetValue(treeIter, 0)=" + listStore.GetValue(treeIter, 0));
+//				Console.WriteLine ("listStore.GetValue(treeIter, 1)=" + listStore.GetValue(treeIter, 1));
+//			} else
+//				Console.WriteLine ("Ninguno seleccionado");
+//		};
 	}
 	
 	private string[] getColumnNames(MySqlDataReader mySqlDataReader) {
--- PTreeView/PTreeView/gtk-gui/MainWindow.cs
+++ PTreeView/PTreeView/gtk-gui/MainWindow.cs
@@ -3,6 +3,10 @@
 
 public partial class MainWindow
 {
+	private global::Gtk.UIManager UIManager;
+	private global::Gtk.Action editAction;
+	private global::Gtk.VBox vbox1;
+	private global::Gtk.Toolbar toolbar;
 	private global::Gtk.ScrolledWindow GtkScrolledWindow;
 	private global::Gtk.TreeView treeView;
 	
@@ -10,10 +14,31 @@ public partial class MainWindow
 	{
 		global::Stetic.Gui.Initialize (this);
 		// Widget MainWindow
+		this.UIManager = new global::Gtk.UIManager ();
+		global::Gtk.ActionGroup w1 = new global::Gtk.ActionGroup ("Default");
+		this.editAction = new global::Gtk.Action ("editAction", global::Mono.Unix.Catalog.GetString ("_Editar"), null, "gtk-edit");
+		this.editAction.ShortLabel = global::Mono.Unix.Catalog.GetString ("_Editar");
+		w1.Add (this.editAction, null);
+		this.UIManager.InsertActionGroup (w1, 0);
+		this.AddAccelGroup (this.UIManager.AccelGroup);
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString ("MainWindow");
 		this.WindowPosition = ((global::Gtk.WindowPosition)(4));
 		// Container child MainWindow.Gtk.Container+ContainerChild
+		this.vbox1 = new global::Gtk.VBox ();
+		this.vbox1.Name = "vbox1";
+		this.vbox1.Spacing = 6;
+		// Container child vbox1.Gtk.Box+BoxChild
+		this.UIManager.AddUiFromString ("<ui><toolbar name='toolbar'><toolitem name='editAction' action='editAction'/></toolbar></ui>");
+		this.toolbar = ((global::Gtk.Toolbar)(this.UIManager.GetWidget ("/toolbar")));
+		this.toolbar.Name = "toolbar";
+		this.toolbar.ShowArrow = false;
+		this.vbox1.Add (this.toolbar);
+		global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.toolbar]));
+		w2.Position = 0;
+		w2.Expand = false;
+		w2.Fill = false;
+		// Container child vbox1.Gtk.Box+BoxChild
 		this.GtkScrolledWindow = new global::Gtk.ScrolledWindow ();
 		this.GtkScrolledWindow.Name = "GtkScrolledWindow";
 		this.GtkScrolledWindow.ShadowType = ((global::Gtk.ShadowType)(1));
@@ -22,7 +47,10 @@ public partial class MainWindow
 		this.treeView.CanFocus = true;
 		this.treeView.Name = "treeView";
 		this.GtkScrolledWindow.Add (this.treeView);
-		this.Add (this.GtkScrolledWindow);
+		this.vbox1.Add (this.GtkScrolledWindow);
+		global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.GtkScrolledWindow]));
+		w4.Position = 1;
+		this.Add (this.vbox1);
 		if ((this.Child != null)) {
 			this.Child.ShowAll ();
 		}
