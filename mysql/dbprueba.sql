7f9be971f8265e3608b3467414fccc2d72fa13fa
luisvelascoserpis <luis.velasco.serpis@gmail.com>
2013-10-15 11:01:54 +0200

    Modificados scripts mysql con foreign

--- mysql/dbprueba.sql
+++ mysql/dbprueba.sql
@@ -1,12 +1,12 @@
-CREATE TABLE articulo (
-  id int(11) primary key NOT NULL AUTO_INCREMENT,
-  nombre varchar(45) NOT NULL,
-  categoria int(11),
-  precio decimal(10,2)
-);
-
 CREATE TABLE categoria (
   id int(11) primary key NOT NULL AUTO_INCREMENT,
   nombre varchar(45) NOT NULL
+);
+CREATE TABLE articulo (
+  id int(11) primary key NOT NULL AUTO_INCREMENT,
+  nombre varchar(45) NOT NULL,
+  categoria int(11) references categoria (id),
+  precio decimal(10,2)
 )
+
 
